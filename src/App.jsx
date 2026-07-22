2fb15054a05649d1183254c1ade630dab0fa8259
Shroii-san <rzalwira23@gmail.com>
2026-07-22 19:17:22 +0700

    add aditional thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Analytics />
     </>
   );
 }
